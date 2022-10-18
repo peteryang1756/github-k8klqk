e8b2e135cd1deac8e34fad533559a4e95bb40e53
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-10-18 21:53:41 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,19 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-   <Layout description="歡迎來到雙龍體育支援中心.">
-      <header className={clsx("hero", styles.heroBanner)}>
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Docusaurus Tutorial - 5min ⏱️
+          </Link>
+        </div>
+      </div>
+    </header>
   );
 }
 
@@ -19,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`{siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <main>
       <>

0b6e7775c556ec8cf4a9902d45b8c7158fab8601
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-10-18 22:25:58 +0800

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
 };
-  plugins: [
-    // require('path').resolve(__dirname, '../../docusaurus-search-local'),
-    require.resolve('@cmfcmf/docusaurus-search-local')
-  ]
+plugins: [
+    [
+      require.resolve("@cmfcmf/docusaurus-search-local"),
+      {
+        // Options here
+      },
+    ],
+  ],
 };

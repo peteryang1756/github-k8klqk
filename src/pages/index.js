db6d27d48d56ffbbb02d0d6934dc489ee147d2a1
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-10-19 21:31:45 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import styles from './index.module.css';
+
 
 import styles from './index.module.css';
 

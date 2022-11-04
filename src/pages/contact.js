be2db652919dd3149cdd0b3aa416668ae497dda8
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-11-05 00:56:19 +0800

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,9 +12,26 @@ export default function Hello() {
           height: '50vh',
           fontSize: '20px',
         }}>
-        <p>
-          <iframe src="https://ssport.surveysparrow.com/s/Contact-Form/tt-viQ3pV8AhXTXds2ypscmCZ?" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
-        iframe>
+        <p><iframe
+  src="https://ssport.surveysparrow.com/s/Contact-Form/tt-viQ3pV8AhXTXds2ypscmCZ?"
+  style={{
+    position: "fixed",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    width: "100%",
+    height: "100%",
+    border: "none",
+    margin: 0,
+    padding: 0,
+    overflow: "hidden",
+    zIndex: 999999
+  }}
+>
+  Your browser doesn't support iframes
+</iframe>
+
 <p>
       </div>
     </Layout>

20d6b1919af778ca20f0d223b48a071a22b949df
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-11-05 00:44:49 +0800

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,9 +13,9 @@ export default function Hello() {
           fontSize: '20px',
         }}>
         <p>
-          <div id="ss_survey_widget"></div>
-  <script id="SS_SCRIPT">function sparrowLaunch(opts){var e="ss-widget",t="script",a=document,r=window,l=localStorage;var s,n,c,rm=a.getElementById('SS_SCRIPT');r.SS_WIDGET_TOKEN="tt-rXeAftBoHPXvEapvtFarHk";r.SS_ACCOUNT="ssport.surveysparrow.com";r.SS_SURVEY_NAME="Contact-Form";if(!a.getElementById(e) && !l.getItem('removed-ss-widget-tt-rXeAftBoHPXvEapvtFarHk')){var S=function(){S.update(arguments)};S.args=[];S.update=function(e){S.args.push(e)};r.SparrowLauncher=S;s=a.getElementsByTagName(t);c=s[s.length-1];n=a.createElement(t);n.type="text/javascript";n.async=!0;n.id=e;n.src=["https://","ssport.surveysparrow.com/widget/",r.SS_WIDGET_TOKEN,"?","customParams=",JSON.stringify(opts)].join("");c.parentNode.insertBefore(n,c);r.SS_VARIABLES=opts;rm.parentNode.removeChild(rm);}};</script>
+          <script id="SS_SCRIPT">function sparrowLaunch(opts){var e="ss-widget",t="script",a=document,r=window,l=localStorage;var s,n,c,rm=a.getElementById('SS_SCRIPT');r.SS_WIDGET_TOKEN="tt-rXeAftBoHPXvEapvtFarHk";r.SS_ACCOUNT="ssport.surveysparrow.com";r.SS_SURVEY_NAME="Contact-Form";if(!a.getElementById(e) && !l.getItem('removed-ss-widget-tt-rXeAftBoHPXvEapvtFarHk')){var S=function(){S.update(arguments)};S.args=[];S.update=function(e){S.args.push(e)};r.SparrowLauncher=S;s=a.getElementsByTagName(t);c=s[s.length-1];n=a.createElement(t);n.type="text/javascript";n.async=!0;n.id=e;n.src=["https://","ssport.surveysparrow.com/widget/",r.SS_WIDGET_TOKEN,"?","customParams=",JSON.stringify(opts)].join("");c.parentNode.insertBefore(n,c);r.SS_VARIABLES=opts;rm.parentNode.removeChild(rm);}};</script>
   <script>sparrowLaunch({/*add custom params here*/})</script>
+<p>
       </div>
     </Layout>
   );

7000774a881696120cb50602734e41f99b926fa7
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-11-05 18:56:26 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,6 @@ import Layout from '@theme/Layout';
 import Hero from "../components/Hero";
 import two from "../components/two.js";
 
-
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
@@ -34,13 +33,8 @@ export default function Home() {
       description="​雙龍職籃 ... 籃球，為一種由兩隊參與，在一個長方形籃球場進行的球類運動。每隊出場5名隊員，可將球向任何方向傳、投、拍、滾或運，目的是將籃球投入對方球籃得分，並阻止 ...">
       <main>
       <>
-        <Hero />
+      <Hero />
          <two />
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 </>
       </main>
     </Layout>

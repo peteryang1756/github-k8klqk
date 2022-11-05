2fb43d7c0e964126757532f0299be14a44918317
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-11-05 18:11:34 +0800

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,43 +7,52 @@ import useBaseUrl from "@docusaurus/useBaseUrl";
 export default function Hero() {
   return (
     <header className={styles.hero}>
-      <div className="container">
-        <div className="row">
-          <div className="col col--6">
-            <h1>
-              A Docker-powered stateless API <br />
-              <span className="highlight">for PDF files</span>
-            </h1>
+  <div className="header">
+    <div className="container">
+      <div className="hero-content">
+        <div className="hero-block">
+          <div className="content-block-hero">
+            <h1 className="heading-2">
+              <strong className="bold-text-9">我們的</strong>
+              <span>
+                <strong className="bold-text">使命</strong>
+              </span>
+              <strong className="bold-text-9">是將</strong>
+              <span>
+                <strong className="bold-text-3">雙</strong>
+              </span>
+              <strong className="bold-text-3">龍</strong>
+              <span>
+                <strong className="bold-text-3">體</strong>
+                <strong className="bold-text-3">育</strong>
+              </span>
+              <strong className="bold-text-10">組成</strong>
+              <span>
+                <strong className="bold-text-6">最棒</strong>
+             </span>
+              <strong className="bold-text-10">體育組織</strong>
+              <span>
+              </span>
             <Link
               className={clsx(
                 "button button--primary button--lg",
                 styles.getStartedButton
               )}
-              to="/docs/get-started/live-demo"
+              to="#123"
             >
-              Get Started
-            </Link>
-            <Link
-              className="button button--secondary button--lg"
-              to="https://github.com/gotenberg/gotenberg"
-            >
+              我們的運動           
               <img
                 className={styles.githubIcon}
-                src={useBaseUrl("/img/github.svg")}
+                src={useBaseUrl("https://74-rho.vercel.app/images/undraw_fitness_stats_sht6.svg")}
                 alt="GitHub Logo"
               />{" "}
               <span className={styles.githubButtonText}>GitHub</span>
-            </Link>
-            <p className="padding-top--md">
-              Available on <strong>amd64</strong>, <strong>arm64</strong>,{" "}
-              <strong>armhf</strong>, and <strong>i386</strong> architectures
-            </p>
-          </div>
+            </Link>           
           <div className="col col--6">
             <img
               className={styles.logo}
-              src={useBaseUrl("/img/logo_jed.png")}
-              alt="Gotenberg Hero Logo"
+              src={useBaseUrl("https://74-rho.vercel.app/images/undraw_fitness_stats_sht6.svg")}
+              alt="ssport"
             />
           </div>
         </div>

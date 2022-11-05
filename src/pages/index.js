aab8bd6d7d64140dd755743ca5abc264bcdb92f4
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-11-05 19:00:13 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import Hero from "../components/Hero";
-import TWO from "../components/two.js";
+import Pulls from "../components/Pulls.js";
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -34,7 +34,7 @@ export default function Home() {
       <main>
       <>
       <Hero />
-         <two />
+         <Pulls />
 </>
       </main>
     </Layout>

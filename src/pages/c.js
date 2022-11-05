ee73dc74cb0c95c4f360df1b329009b59deff572
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-11-05 22:15:05 +0800

    Create c.js

---
+++ src/pages/c.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function Hello() {
+  return (
+    <Layout title="聯繫" description="有任何問題嗎？我們很樂意聽取您的意見。向我們發送訊息，我們會儘快回復。">
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '50vh',
+          fontSize: '20px',
+        }}>
+        <p>
+<iframe
+  src="https://ssport.surveysparrow.com/s/Contact-Form/tt-viQ3pV8AhXTXds2ypscmCZ?"
+  width="100%"
+  height="100%"
+  style={{ border: "1px solid black" }}
+></iframe>
+        </p>
+      </div>
+    </Layout>
+  );
+}

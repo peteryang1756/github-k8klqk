3abe83c5437f855b18232933a6f6e319ae9a208e
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-11-05 18:24:04 +0800

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,53 +7,31 @@ import useBaseUrl from "@docusaurus/useBaseUrl";
 export default function Hero() {
   return (
     <header className={styles.hero}>
-  <div className="header">
-    <div className="container">
-      <div className="hero-content">
-        <div className="hero-block">
-          <div className="content-block-hero">
-            <h1 className="heading-2">
-              <strong className="bold-text-9">我們的</strong>
-              <span>
-                <strong className="bold-text">使命</strong>
-              </span>
-              <strong className="bold-text-9">是將</strong>
-              <span>
-                <strong className="bold-text-3">雙</strong>
-              </span>
-              <strong className="bold-text-3">龍</strong>
-              <span>
-                <strong className="bold-text-3">體</strong>
-                <strong className="bold-text-3">育</strong>
-              </span>
-              <strong className="bold-text-10">組成</strong>
-              <span>
-                <strong className="bold-text-6">最棒</strong>
-             </span>
-              <strong className="bold-text-10">體育組織</strong>
-              <span>
-              </span>
-            <Link
+      <div className="container">
+        <div className="row">
+          <div className="col col--6">
+            <h1>
+             我們的使命是將雙龍體育組成最棒 <br />
+              <span className="highlight">體育組織</span>
+            </h1>
+                        <Link
               className={clsx(
                 "button button--primary button--lg",
                 styles.getStartedButton
               )}
               to="#123"
             >
-              我們的運動           
-              <img
-                className={styles.githubIcon}
-                src={useBaseUrl("https://74-rho.vercel.app/images/undraw_fitness_stats_sht6.svg")}
-                alt="GitHub Logo"
-              />{" "}
-              <span className={styles.githubButtonText}>GitHub</span>
-            </Link>           
+              我們的運動
+            </Link>            
           <div className="col col--6">
             <img
               className={styles.logo}
               src={useBaseUrl("https://74-rho.vercel.app/images/undraw_fitness_stats_sht6.svg")}
-              alt="ssport"
+              alt="Gotenberg Hero Logo"
             />
+          </div>
+        </div>
+      </div>
     </header>
   );
 }

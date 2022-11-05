323c72abc58a4a1fb8c4286a813c100241425e58
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-11-05 22:23:51 +0800

    Update c.js

--- src/pages/c.js
+++ src/pages/c.js
@@ -15,7 +15,7 @@ export default function Hello() {
         <p>
 <iframe
   width="100%"
-  height="800px"
+  height="formHeight"
   id="iframe"
   scrolling="no"
   src="https://ssport.surveysparrow.com/s/Contact-Form/tt-viQ3pV8AhXTXds2ypscmCZ?"

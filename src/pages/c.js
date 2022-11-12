fceb3f9bc87c7e995052062afa6f83901447f78c
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-11-13 01:09:52 +0800

    Update c.js

--- src/pages/c.js
+++ src/pages/c.js
@@ -3,7 +3,7 @@ import Layout from '@theme/Layout';
 
 export default function Hello() {
   return (
-    <Layout title="聯絡" description="聯絡我們">
+    <Layout title="聯繫" description="聯繫雙龍體育">
       <div
         style={{
           display: 'flex',
@@ -13,119 +13,64 @@ export default function Hello() {
           fontSize: '20px',
         }}>
         <p>
-   <>
-  <p>&nbsp;</p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-</>
- <>
-  <p>&nbsp;</p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-</>
-     
-    
-    {/* Author: FormBold Team */}
-    {/* Learn More: https://formbold.com */}
-    <div className="formbold-form-wrapper">
-      <form action="https://formbold.com/s/FORM_ID" method="POST">
-        <div className="formbold-mb-5">
-          <label htmlFor="name" className="formbold-form-label">
-            {" "}
-            Full Name{" "}
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Full Name"
-            className="formbold-form-input"
-          />
-        </div>
-        <div className="formbold-mb-5">
-<>
-  <div className="formbold-main-wrapper">
-    {/* Author: FormBold Team */}
-    {/* Learn More: https://formbold.com */}
-    <div className="formbold-form-wrapper">
-      <form action="https://formbold.com/s/FORM_ID" method="POST">
-        <div className="formbold-mb-5">
-          <label htmlFor="name" className="formbold-form-label">
-            {" "}
-            Full Name{" "}
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Full Name"
-            className="formbold-form-input"
-          />
-        </div>
-        <div className="formbold-mb-5">
-          <label htmlFor="email" className="formbold-form-label">
-            {" "}
-            Email Address{" "}
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Enter your email"
-            className="formbold-form-input"
-          />
-        </div>
-        <div className="formbold-mb-5">
-          <label htmlFor="subject" className="formbold-form-label">
-            {" "}
-            Subject{" "}
-          </label>
-          <input
-            type="text"
-            name="subject"
-            id="subject"
-            placeholder="Enter your subject"
-            className="formbold-form-input"
-          />
-        </div>
-        <div className="formbold-mb-5">
-          <label htmlFor="message" className="formbold-form-label">
-            {" "}
-            Message{" "}
-          </label>
-          <textarea
-            rows={6}
-            name="message"
-            id="message"
-            placeholder="Type your message"
-            className="formbold-form-input"
-            defaultValue={""}
-          />
-        </div>
-        <div>
-          <button className="formbold-btn">Submit</button>
-        </div>
-      </form>
-    </div>
+          <div class="formbold-main-wrapper">
+  <!-- Author: FormBold Team -->
+  <!-- Learn More: https://formbold.com -->
+  <div class="formbold-form-wrapper">
+    <form action="https://formbold.com/s/FORM_ID" method="POST">
+      <div class="formbold-mb-5">
+        <label for="name" class="formbold-form-label"> Full Name </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Full Name"
+          class="formbold-form-input"
+        />
+      </div>
+
+      <div class="formbold-mb-5">
+        <label for="email" class="formbold-form-label"> Email Address </label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Enter your email"
+          class="formbold-form-input"
+        />
+      </div>
+
+      <div class="formbold-mb-5">
+        <label for="subject" class="formbold-form-label"> Subject </label>
+        <input
+          type="text"
+          name="subject"
+          id="subject"
+          placeholder="Enter your subject"
+          class="formbold-form-input"
+        />
+      </div>
+
+      <div class="formbold-mb-5">
+        <label for="message" class="formbold-form-label"> Message </label>
+        <textarea
+          rows="6"
+          name="message"
+          id="message"
+          placeholder="Type your message"
+          class="formbold-form-input"
+        ></textarea>
+      </div>
+
+      <div>
+        <button class="formbold-btn">Submit</button>
+      </div>
+    </form>
   </div>
-  <style dangerouslySetInnerHTML={{ __html: "\n" }} />
-</>
-
-
-
-  <link rel="stylesheet" href="https://ssangyongsports.github.io/1.css" />
 </div>
+<style>
+        </p>
+      </div>
     </Layout>
   );
 }

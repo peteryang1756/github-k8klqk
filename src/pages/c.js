f3567aba51187785812e8a42a6993b71149c9d62
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-11-13 10:02:44 +0800

    Update c.js

--- src/pages/c.js
+++ src/pages/c.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '@theme/Layout';
+import Hero from "../components/Hero";
 
 export default function Hello() {
   return (
@@ -13,109 +14,7 @@ export default function Hello() {
           fontSize: '20px',
         }}>
         <p>
-    <>
-  <p>&nbsp;</p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-</>
-
-          <div class="formbold-main-wrapper">
-  <div class="formbold-form-wrapper">
-    <form action="https://formbold.com/s/FORM_ID" method="POST">
-      <div class="formbold-mb-5">
-        <>
-  <p>&nbsp;</p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-</>
-
-        <label for="name" class="formbold-form-label"> Full Name </label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Full Name"
-          class="formbold-form-input"
-        />
-      </div>
-
-      <div class="formbold-mb-5">
-        <label for="email" class="formbold-form-label"> Email Address </label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Enter your email"
-          class="formbold-form-input"
-        />
-      </div>
-
-      <div class="formbold-mb-5">
-        <label for="subject" class="formbold-form-label"> Subject </label>
-        <input
-          type="text"
-          name="subject"
-          id="subject"
-          placeholder="Enter your subject"
-          class="formbold-form-input"
-        />
-      </div>
-
-      <div class="formbold-mb-5">
-        <label for="message" class="formbold-form-label"> Message </label>
-        <textarea
-          rows="6"
-          name="message"
-          id="message"
-          placeholder="Type your message"
-          class="formbold-form-input"
-        ></textarea>
-      </div>
-
-      <div>
-        <button class="formbold-btn">Submit</button>
-<>
-  <p>&nbsp;</p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-</>
-      </div>
-    </form>
-  </div>
-</div>
-
-
-<link rel="stylesheet" href="https://ssangyongsports.github.io/1.css" />
+          <Hero />
         </p>
       </div>
     </Layout>

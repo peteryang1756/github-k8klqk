16aa3c3011a099415d73d273ee71c9aab189d39e
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-11-13 10:58:24 +0800

    Update c.js

--- src/pages/c.js
+++ src/pages/c.js
@@ -3,7 +3,7 @@ import Layout from '@theme/Layout';
 
 export default function Hello() {
   return (
-    <Layout title="聯繫" description="聯繫雙龍體育">
+    <Layout title="聯絡" description="聯絡我們">
       <div
         style={{
           display: 'flex',
@@ -13,7 +13,97 @@ export default function Hello() {
           fontSize: '20px',
         }}>
         <p>
-         build
+   <>
+  <p>&nbsp;</p>
+  <p>
+    <br />
+  </p>
+  <p>
+    <br />
+  </p>
+</>
+ <>
+  <p>&nbsp;</p>
+  <p>
+    <br />
+  </p>
+  <p>
+    <br />
+  </p>
+  <p>
+    <br />
+  </p>
+</>
+     
+    <div className="example-form example-form_before">
+  <div className="example-title">聯絡</div>
+  <form
+    method="post"
+    action="https://send.pageclip.co/uLHUxztMTNIK2IljRIEgBJh6JVi7QruG/Site-contact"
+  >
+    <div className="form-group">
+      <label>名稱</label>
+      <input
+        className="form-control"
+        type="text"
+        name="name"
+        placeholder="peter"
+       required
+      />
+      <label>電子郵件</label>
+      <input
+        required=""
+        className="form-control"
+        type="email"
+        name="email"
+        data-valuemissing="Need an email!"
+        data-patternmismatch="Only a and b characters allowed!"
+        placeholder="test@test.com"
+       required
+      />
+    </div>
+    <div className="form-group">
+      <label>內容</label>
+      <textarea
+        required=""
+        className="form-control"
+        name="內容"
+        pattern="[ab ]+"
+        rows={4}
+        data-valuemissing="Write me a story plz"
+        data-patternmismatch="Plz only a and b characters!"
+        placeholder="Tell me a story!"
+        defaultValue={""}
+       required
+      />
+    </div>
+    <button type="submit">Submit</button>
+  </form>
+   <>
+  <p>&nbsp;</p>
+  <p>
+    <br />
+  </p>
+  <p>
+    <br />
+  </p>
+</>
+<>
+  <p>&nbsp;</p>
+  <p>
+    <br />
+  </p>
+  <p>
+    <br />
+  </p>
+  <p>
+    <br />
+  </p>
+</>
+
+  <link rel="stylesheet" href="https://ssangyongsports.github.io/1.css" />
+</div>
+
         </p>
       </div>
     </Layout>

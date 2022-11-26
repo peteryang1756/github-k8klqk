8f112892a90f55fdd658bbdca9b77a4cbb2fa888
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-11-27 00:18:00 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,6 +59,7 @@ const config = {
         logo: {
           alt: '雙龍體育 Logo',
           src: 'https://img.ssangyongsports.eu.org/logo2.png',
+          style: {backgroundColor: 'transparent'},
         },
         items: [
           {to: '/blog', label: 'Blog', position: 'left'},
